60ee778bd5e14537e072a9617c274f2de0f8c24e
Angie Smith <avksmit2@gmail.com>
2016-09-27 11:55:36 -0700

    add the basic home page and a page for the courses.

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Task.php";
-    require_once __DIR__."/../src/Category.php";
+    require_once __DIR__."/../src/Course.php";
+    require_once __DIR__."/../src/Student.php";
 
     use Symfony\Component\Debug\Debug;
     Debug::enable();
@@ -13,19 +13,39 @@
 
     $app['debug'] = true;
 
-    $server = 'mysql:host=localhost;dbname=to_do';
+    $server = 'mysql:host=localhost;dbname=registrar';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
-
-
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
-      return $app['twig']->render("index.html.twig");
+        return $app['twig']->render("index.html.twig");
+    });
+
+    $app->get("/courses", function() use ($app) {
+        return $app['twig']->render("courses.html.twig", array('courses' => Course::getAll()));
+    });
+
+    $app->post("/courses_add", function() use ($app) {
+        $course_name = $_POST['course_name'];
+        $course_number = $_POST['course_number'];
+        $course = new Course($course_name, $course_number);
+
+        return $app['twig']->render("courses.html.twig", array('courses' => Course::getAll()));
+    });
+
+    $app->get("/courses_delete", function() use ($app) {
+        Course::deleteAll();
+
+        return $app['twig']->render("courses.html.twig");
+    });
+
+    $app->get("/students", function() use ($app) {
+        return $app['twig']->render("students.html.twig", array('students' => Student::getAll()));
     });
 
     return $app;
